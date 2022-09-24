8382c40e438914e8aac7b7a89571c2c3e0fce791
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-25 01:04:28 +0300

    add navigate + handle submit for search form

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { FiSearch } from 'react-icons/fi';
 
 const Searchbar = () => {
+  const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => { e.preventDefault(); navigate(`/search/${searchTerm}`); };
 
   return (
     <form onSubmit={handleSubmit} autoComplete='off' className='p-2 text-gray-400 focus-within:text-gray-600'>
