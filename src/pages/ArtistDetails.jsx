f5557721b41c7de1f22e9bfa8776f2ea1ab8a610
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-25 00:10:21 +0300

    fix typos

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -7,9 +7,10 @@ import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 const ArtistDetails = () => {
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery({ artistId });
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-  if (isFetchingArtistDetails) return <Loader title='Searching artist details...' />;
+  if (isFetchingArtistDetails) return <Loader title='Loading artist details...' />;
+
   if (error) return <Error />;
 
   return (
@@ -17,7 +18,7 @@ const ArtistDetails = () => {
       <DetailsHeader artistId={artistId} artistData={artistData} />
 
       <RelatedSongs
-        data={Object.values(artistData?.song)}
+        data={Object.values(artistData?.songs)}
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
