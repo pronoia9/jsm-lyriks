1794050be11491798cb5f8ad7f1640e2cf8a0813
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 20:20:09 +0300

    add data from get top charts query

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,14 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
 const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
+  if (isFetching) return <Loader title='Loading songs' />;
+  if (error) return <Error />;
   return (
     <div className='flex flex-col'>
       <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
@@ -21,8 +26,8 @@ const Discover = () => {
       </div>
 
       <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-        {Array.from({ length: 10 }, (val, i) => i).map((song, i) => 
-        <SongCard key={song} song={song} i={i} />
+        {data?.map((song, i) => 
+        <SongCard key={i} song={song} i={i} />
         )}
       </div>
     </div>
