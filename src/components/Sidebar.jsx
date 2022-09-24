608e0ac29ba021bad645a558ba56f96f5dacbb74
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 21:30:49 +0300

    add navlinks component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,12 +12,28 @@ const links = [
   { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
 ];
 
+const NavLinks = () => (
+  <div className='mt-10'>
+    {links.map((item, i) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+        onClick={() => handleClick && handleClick()}>
+        <item.icon className='w-6 h-6 mr-2' />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
 const Sidebar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
     <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
       <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+      <NavLinks />
     </div>
   );
 };
