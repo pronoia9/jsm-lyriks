becbf11c752428b047294d6bb637933223d67f34
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 21:23:06 +0300

    add logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,25 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup, } from 'react-icons/hi';
+
+import { logo } from '../assets';
+
+const links = [
+  { name: 'Discover', to: '/', icon: HiOutlineHome },
+  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+];
+
+const Sidebar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  return (
+    <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
+      <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+    </div>
+  );
+};
 
 export default Sidebar;
