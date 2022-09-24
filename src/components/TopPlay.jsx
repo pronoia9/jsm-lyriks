13c98a519c07d75c18722330514fa272f29ba9ff
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 22:22:48 +0300

    update class for top chart card

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,10 +12,13 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className='w-full flex flex-row items-center hover:bg-[#4c426e py-2 p-4 rounded-lg cursor-pointer mb-2'>
-    <h3 className='font-bold text-base text-white mr-3'>{i + 1}. </h3>
+  <div
+    className={`w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 ${
+      activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'
+    }`}>
+    <h3 className='font-bold text-base text-white mr-3'>{i + 1}.</h3>
     <div className='flex-1 flex flex-row justify-between items-center'>
-      <img src={song?.images?.coverart} alt={song?.images?.coverart} className='w-20 h-20 rounded-lg' />
+      <img src={song?.images?.coverart} alt={song?.title} className='w-20 h-20 rounded-lg' />
       <div className='flex-1 flex flex-col justify-center mx-3'>
         <Link to={`/songs/${song.key}`}>
           <p className='text-xl font-bold text-white'>{song?.title}</p>
@@ -88,7 +91,7 @@ const TopPlay = () => {
           centeredSlidesBounds
           modules={[FreeMode]}
           className='mt-4'>
-          {topPlays?.map((song) => (
+          {topPlays?.slice(0, 5).map((song) => (
             <SwiperSlide
               key={song?.key}
               style={{ width: '25%', height: 'auto' }}
