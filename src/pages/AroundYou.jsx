3a04af746d58f4a2a6f77dd901e77412d88ec518
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-25 00:42:56 +0300

    finish around you page

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -22,9 +22,18 @@ const AroundYou = () => {
       .finally(() => setLoading(false));
   }, [country]);
 
-  if (isFetching && loading) return <Loader title='Loading songs around you...' />
-  if (error) return <Error />
-  return <div className='flex flex-col'></div>;
+  if (isFetching && loading) return <Loader title='Loading songs around you...' />;
+  if (error) return <Error />;
+  return (
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>Around You <span className='font-black'>{country}</span></h2>
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((song, i) => (
+          <SongCard key={song.key} song={song} isPlaying={isPlaying} activeSong={activeSong} data={data} i={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AroundYou;
