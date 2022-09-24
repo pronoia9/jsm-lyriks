ed75d4508dac4a4d95761185bdb517c897793a90
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 22:09:17 +0300

    add top artists swiper

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -33,16 +33,49 @@ const TopPlay = () => {
   return (
     <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
       <div className='w-full flex flex-col'>
+        {/* Top charts */}
         <div className='flex flex-row justify-between items-center'>
           <h2 className='text-white font-bold text-2xl'>Top Charts</h2>
           <Link to='/top-charts'>
             <p className='text-gray-300 text-base cursor-pointer'>See more</p>
           </Link>
         </div>
-
+        {/* Top charts songs */}
         <div className='mt-4 flex flex-col gap-1'>
-          {topPlays?.slice(0, 5).map((song, i) => <TopChartCard key={song.key} song={song} i={i} />)}
+          {topPlays?.map((song, i) => (
+            <TopChartCard key={song.key} song={song} i={i} />
+          ))}
         </div>
+      </div>
+
+      {/* Top artists */}
+      <div className='w-full flex flex-col mt-8'>
+        <div className='flex flex-row justify-between items-center'>
+          <h2 className='text-white font-bold text-2xl'>Top Artists</h2>
+          <Link to='/top-artists'>
+            <p className='text-gray-300 text-base cursor-pointer'>See more</p>
+          </Link>
+        </div>
+        {/* Top artists swiper */}
+        <Swiper
+          slidesPerView='auto'
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className='mt-4'>
+          {topPlays?.map((artist) => (
+            <SwiperSlide
+              key={artist?.key}
+              style={{ width: '25%', height: 'auto' }}
+              className='shadow-lg rounded-full animate-slideright'>
+              <Link to={`/artists/${artist?.artists[0].adamid}`}>
+                <img src={artist?.images?.background} alt='Name' className='rounded-full w-full object-cover' />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
