adebbba1e3f85780d437bc7cbde45e09cc4e51ea
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-24 19:42:36 +0300

    add shazam core api call

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '39a0cb0b13msh6f8fe7cc19b5aaep146db1jsn65a41ec2ed65',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+  },
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({ reducerPath: 'shazamCoreApi' });
