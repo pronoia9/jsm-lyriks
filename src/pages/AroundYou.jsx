403fcae60a3c4018332cb8d7062fb8e34fdf784d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-25 00:39:47 +0300

    add geolocation api

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
-export default CountryTracks;
+const AroundYou = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+  console.log(country);
+  console.log(data);
+
+  useEffect(() => {
+    axios
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  if (isFetching && loading) return <Loader title='Loading songs around you...' />
+  if (error) return <Error />
+  return <div className='flex flex-col'></div>;
+};
+
+export default AroundYou;
